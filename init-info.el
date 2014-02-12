362f0ce6d15f886cd726d1fde7703c9a6fd3af3c
Zhiyong Ma <ccdevote@gmail.com>
2014-02-12 16:43:14 +0800

    add personal info

--- init-info.el
+++ init-info.el
@@ -1,5 +1,7 @@
 ;; my favourite configuration;;
 ;; personal info
+(define-auto-insert 'cc-mode "template/c.tpl")
+(add-hook 'find-file-hooks 'auto-insert)
 (setq user-full-name "Zhiyong Ma")
 (setq user-mail-address "ccdevote@gmail.com")
 (setq mail-replay-to "ccdevote@gmail.com");
